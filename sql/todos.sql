6599827884249cfd8bc6e93816300d289fa27902
Matt Saragusa <polyhedron@gmail.com>
2018-03-04 17:22:02 -0600

    Added sql dump file

---
+++ sql/todos.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 05, 2018 at 12:20 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.0.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `todos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `todoID` int(11) NOT NULL,
+  `userID` varchar(8) NOT NULL,
+  `text` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`todoID`, `userID`, `text`) VALUES
+(1, 'bc9X', 'Get Yogurt'),
+(2, 'bc9X', 'Get Bread'),
+(3, 'bc9X', 'Get Ketchup'),
+(4, 'bc9X', 'Get Milk');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`todoID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `todoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
