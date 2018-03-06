7b820c5fcc471e59d984f9a3fad06325d679cee5
Matt Saragusa <polyhedron@gmail.com>
2018-03-05 21:19:43 -0600

    added database lookup per userID into todos.php. This connects to the database and returns the user's todo information

--- ajax/todos.php
+++ ajax/todos.php
@@ -1,6 +1,26 @@
 <?php
-$todos[] = 'Get Cheetos';
-$todos[] = 'Get Mountain Dew';
-$todos[] = 'Get Mello Yellow';
+
+$username = 'root';
+$password = '';
+
+try {
+
+$dbh = new PDO('mysql:host=localhost;dbname=todos', $username, $password);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$stmt = $dbh->prepare("SELECT text FROM todos where userID = ?");
+
+
+$todos = array();
+if ($stmt->execute(array($_GET['userID']))) {
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $todos[] = $row['text'];
+    }
+}
 
 echo json_encode($todos);
